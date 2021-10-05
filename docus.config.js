a3a05453d4b18817825afd23faa04d919a206ff7
GrahamSH <grahamshllk@gmail.com>
2021-10-05 08:39:28 -0400

    Update docus.config.js

--- docus.config.js
+++ docus.config.js
@@ -13,7 +13,8 @@ export default {
   theme: {
     logo: '/icon.png',
     shortcuts: {
-      'd-header': "p-2 bg-red-100 flex justify-between h-[57.5px] print:hidden"
+      'd-header': "p-2 bg-red-100 flex justify-between h-[57.5px] print:hidden",
+      'd-container-content': 'ml-0 mr-0 justify-between'
     },
     header: {
       logo: '/icon.png',
